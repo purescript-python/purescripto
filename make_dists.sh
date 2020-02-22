764cdc4250e9c40e11f81916392dda67304ffb63
thautawarm <twshere@outlook.com>
2020-02-22 21:41:50 +0900

    make distribution

--- make_dists.sh
+++ make_dists.sh
@@ -1,5 +1,5 @@
 for PLAT in "win64" "linux64" "macosx_10_15" ; do
-  for CPY in "37" ; do
+  for CPY in "35" "36" "37" "38" ; do
       export CPY=$CPY
       export PLAT=$PLAT
       python setup.py bdist_wheel
