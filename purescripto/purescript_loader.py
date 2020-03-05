c4e42979e64dd2fce128842c455041dde62fc272
thautawarm <twshere@outlook.com>
2020-03-05 18:11:09 +0900

    update for purescript-python 0.1.2

--- purescripto/purescript_loader.py
+++ purescripto/purescript_loader.py
@@ -7,23 +7,25 @@ from purescripto.topdown import load_topdown
 from purescripto.rts import META_ENV, RTS_TEMPLATE
 import marshal
 import zipfile
+import ast
 import io
-import sys
+
 
 class LoadPureScriptImplCode(LoaderForBetterLife[CodeType]):
     def source_to_prog(self, src: bytes, path: Path) -> CodeType:
-        sys.setrecursionlimit(3000)
         filename = str(path.absolute())
         if path.name.endswith(".raw.py"):
-            meta_code = compile(src, filename, "eval")
+            expr = ast.Expression(ast.parse(src).body[0].value)
+            src_path, meta_code = compile(expr, filename, "eval")
             sexpr = eval(meta_code, META_ENV)
         else:
             assert path.name.endswith(".zip.py")
             zip = zipfile.ZipFile(filename)
+            src_path = zip.read("srcpath").decode('utf8')
             file = io.StringIO(zip.read("source").decode("utf8"))
             sexpr = load_topdown(file, META_ENV)
 
-        code = module_code(sexpr, name=self.qualified_name + "$", filename=filename)
+        code = module_code(sexpr, name=self.qualified_name + "$", filename=src_path)
         return code
 
     def load_program(self, b: bytes) -> CodeType:
@@ -33,7 +35,7 @@ class LoadPureScriptImplCode(LoaderForBetterLife[CodeType]):
         return marshal.dumps(prog)
 
     def suffix(self) -> Union[str, Tuple[str, ...]]:
-        return ".raw.py", ".zip.py"
+        return ".zip.py", ".raw.py"
 
 
 def LoadPureScript(file: str, name: str):
